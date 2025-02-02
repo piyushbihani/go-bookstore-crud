97936be4a35e08d1d1d3b40f034fc8308fcaa01f
piyushbihani <piyushbihani.pb28@gmail.com>
2025-02-02 15:33:49 +0530

    Hiding DB credentials

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -3,12 +3,19 @@ package config
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
+	"fmt"
 )
 
 var DB *gorm.DB
 
 func ConnectDataBase() {
-	dsn := "Piyush:Piyush@2002@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	if dbUser == "" || dbPass == "" {
+		panic("Error: Environment variables DBUserName or DBPassword are not set")
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database: " + err.Error())
